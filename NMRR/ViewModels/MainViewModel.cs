fc3575a4723bede869f107f468447afa737f141f
Ali Pourostad <ali.pourostad@ut.ac.ir>
2024-12-16 19:34:51 +0330

    Correct Receiving and plotting 2 channels

--- NMRR/ViewModels/MainViewModel.cs
+++ NMRR/ViewModels/MainViewModel.cs
@@ -6,6 +6,8 @@ using NMRR.Models;
 using System.Windows.Input;
 using NMRR.Helpers;
 using System.IO;
+using LiveCharts;
+using System.Linq;
 
 namespace NMRR.ViewModels
 {
@@ -19,6 +21,14 @@ namespace NMRR.ViewModels
         public ObservableCollection<DeviceModel> DataPoints { get; set; }
         public double[] ADC_Buffer;
         public uint[] Time_Buffer;
+
+        private List<double> tPosCsv;
+        private List<double> tTqCsv;
+        private List<double> PosCsv;
+        private List<double> TqCsv;
+
+        public ChartValues<double> PosValues { get; set; } // Values for PosValue
+        public ChartValues<double> TqValues { get; set; }  // Values for TqValue
         public string CommandToSend { get; set; }
         public string SerialLog { get; set; } = string.Empty;
 
@@ -35,6 +45,9 @@ namespace NMRR.ViewModels
             _serialPortService = new SerialPortService();
             DataPoints = new ObservableCollection<DeviceModel>();
 
+            PosValues = new ChartValues<double>(); // Initialize PosValue chart
+            TqValues = new ChartValues<double>();  // Initialize TqValue chart
+
             StartCommand = new RelayCommand(StartReceiving);
             StopCommand = new RelayCommand(StopReceiving);
             SendCommand = new RelayCommand(SendCommandToDevice);
@@ -43,12 +56,22 @@ namespace NMRR.ViewModels
             _serialPortService.DataReceived += OnDataReceived;
             ADC_Buffer = new double[50];
             Time_Buffer = new uint[50];
+
+            tPosCsv = new List<double>();
+            PosCsv = new List<double>();
+            tTqCsv = new List<double>();
+            TqCsv = new List<double>();
         }
 
         private void StartReceiving()
         {
             ADC_Buffer = new double[50];
             Time_Buffer = new uint[50];
+            PosValues.Clear(); // Clear PosValue chart
+            TqValues.Clear();  // Clear TqValue chart
+            tTqCsv.Clear();
+            tPosCsv.Clear();
+
             DataPoints.Clear();
             _serialPortService.StartReceiving();
             
@@ -74,9 +97,9 @@ namespace NMRR.ViewModels
 
             csvContent.AppendLine("Pos_t,Pos Value,Tq_t,Tq Value");
 
-            foreach (var data in DataPoints)
+            for (int i = 0; i < tPosCsv.Count; i++)
             {
-                csvContent.AppendLine($"{data.Time_us},{data.PosValue},{data.Tq_t},{data.TqValue}");
+                csvContent.AppendLine($"{tPosCsv[i]},{PosCsv[i]},{tTqCsv[i]},{TqCsv[i]}");
             }
 
             File.WriteAllText(filePath, csvContent.ToString());
@@ -92,22 +115,58 @@ namespace NMRR.ViewModels
             }
             else if(command == "fdb")
             {
+                // Temporary lists to hold batch updates
+                var tPosBatch = new List<double>();
+                var tTqBatch = new List<double>();
+                var posBatch = new List<double>();
+                var tqBatch = new List<double>();
+
                 for (int i = 0; i < ADC_BUFFER_LENGTH; i++)
                 {
                     uint pos_val = BitConverter.ToUInt32(data, ADC_BUFFER_LENGTH * sizeof(uint) + i * sizeof(uint));
                     uint tq_val = BitConverter.ToUInt32(data, 3 * ADC_BUFFER_LENGTH * sizeof(uint) + i * sizeof(uint));
-                    App.Current.Dispatcher.Invoke(() =>
-                    {
-                        DataPoints.Add(new DeviceModel
-                        {
-                            Time_us = BitConverter.ToUInt32(data, i * sizeof(uint)),
-                            PosValue = ((double)pos_val / (1 << 23) - 1) * 25,
-                            Tq_t = BitConverter.ToUInt32(data, 2 * ADC_BUFFER_LENGTH * sizeof(uint) + i * sizeof(uint)),
-                            TqValue = ((double)tq_val / (1 << 23) - 1) * 25
-                            //ADCValue = (double)val
-                        });
-                    });
+
+                    double posValue = ((double)pos_val / (1 << 23) - 1) * 25;
+                    double tqValue = ((double)tq_val / (1 << 23) - 1) * 25;
+
+                    // Add to batch
+                    tPosBatch.Add((double)(BitConverter.ToUInt32(data, i * sizeof(uint)))/1000);
+                    tTqBatch.Add((double)(BitConverter.ToUInt32(data, 2 * ADC_BUFFER_LENGTH * sizeof(uint) + i * sizeof(uint)))/1000);
+                    posBatch.Add(posValue);
+                    tqBatch.Add(tqValue);
+
                 }
+
+                tPosCsv.AddRange(tPosBatch);
+                tTqCsv.AddRange(tTqBatch);
+                PosCsv.AddRange(posBatch);
+                TqCsv.AddRange(tqBatch);
+
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    PosValues.Clear();
+                    TqValues.Clear();
+                    //if (PosValues.Count > 100) PosValues.RemoveAt(0);
+                    //if (TqValues.Count > 100) TqValues.RemoveAt(0);
+                    // Add batch data to charts
+                    PosValues.AddRange(posBatch);
+                    TqValues.AddRange(tqBatch);
+
+
+                    // Notify the UI of property changes
+                    OnPropertyChanged(nameof(PosValues));
+                    OnPropertyChanged(nameof(TqValues));
+
+                    //DataPoints.Add(new DeviceModel
+                    //{
+                    //    Time_us = BitConverter.ToUInt32(data, i * sizeof(uint)),
+                    //    PosValue = posV,
+                    //    Tq_t = BitConverter.ToUInt32(data, 2 * ADC_BUFFER_LENGTH * sizeof(uint) + i * sizeof(uint)),
+                    //    TqValue = tqValue
+                    //    //ADCValue = (double)val
+                    //});
+
+                });
             }
         }
         private void OnDataReceived2(string command, byte[] data)
